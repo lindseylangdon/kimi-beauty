fba0bf01c89e33ead8308b39dc496e7ea42afdfd
lindseylangdon <94773899+lindseylangdon@users.noreply.github.com>
2024-02-25 11:42:25 -0600

    Added Footer, Header, and Announcement components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Announcements from './Components/Announcements';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Announcements />
+        <header className="App-header">
+            <Header />
+        </header>
+      <Footer />
     </div>
   );
 }
