293356ee00c2b33a2c7a61ba968cdb839528778d
lindseylangdon <94773899+lindseylangdon@users.noreply.github.com>
2024-03-08 11:30:14 -0600

    SearchBar cleanup

--- src/Components/Header/NavBar/SearchBar.tsx
+++ src/Components/Header/NavBar/SearchBar.tsx
@@ -20,16 +20,16 @@ export default function SearchBar() {
     return (
         <div className="relative">
             {isExpanded ? (
-                <form onSubmit={handleSearch} className="flex items-center border-b border-gray-700 mt-2">
+                <form onSubmit={handleSearch} className="flex items-center border-b border-gray-700">
                     <input
                         ref={searchInputRef}
                         type="text"
                         placeholder="Search..."
-                        className="text-base lg:text-base md:text-base sm:text-sm text-gray-700 focus:outline-none font-mono px-1 py-1 lg:px-2 md:px-2 fixed inset-0"
+                        className="mt-2 text-base lg:text-base md:text-base sm:text-sm text-gray-700 focus:outline-none font-mono px-1 py-1 lg:px-2 md:px-2"
                         onBlur={() => setIsExpanded(false)}
                     />
                     <button type="submit" 
-                        className="lg:text-base md:text-base sm:text-sm text-gray-700 hover:text-gray-500 focus:outline-none font-mono px-2 transition-all duration-500 cursor-pointer"
+                        className="mt-2 text-base lg:text-base md:text-base sm:text-sm text-gray-700 hover:text-gray-500 focus:outline-none font-mono px-2 transition-all duration-500 cursor-pointer"
                     >
                         enter
                     </button>
