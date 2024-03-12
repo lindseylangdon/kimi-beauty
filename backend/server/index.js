86556a91780711b7f4361570d722bc892cf4e198
lindseylangdon <94773899+lindseylangdon@users.noreply.github.com>
2024-03-12 15:37:35 -0500

    Database authentication now working

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const mongodb = require("mongodb");
 
@@ -5,13 +7,24 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const MongoClient = mongodb.MongoClient;
-const url = "mongodb://localhost:27017/inventory";
+// const url = "mongodb://localhost:27017/inventory";
+const url = `mongodb://${process.env.REACT_APP_MONGO_USER}:${encodeURIComponent(process.env.REACT_APP_MONGO_PASS)}@127.0.0.1:27017/inventory?authSource=inventory`;
 
-// MongoClient.connect(url, function(err, db) {
-//     if (err) throw err;
-//     console.log("Database connected!");
-//     db.close();
-//   });
+MongoClient.connect(url).then(client => {
+    console.log("MongoDB connection successful.");
+
+    const dbo = client.db("inventory");
+    dbo.collection("items").insertOne({ name: "Item1", quantity: 100 }).then(res => {
+        console.log("1 document inserted");
+        client.close();
+    }).catch(err => {
+        console.error("Insert error: ", err);
+        client.close();
+    });
+}).catch(err => {
+    console.error("Connection error: ", err);
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from "../Components/Footer/Footer";
 import Header from '../Components/Header/Header';
 import HomeBody from '../Components/Body/HomeBody';
 
 export default function Home() {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+    // const [data, setData] = useState (null);
+
+    // React.useEffect(() => {
+    //     fetch("/api")
+    //         .then((res) => res.json())
+    //         .then((data) => setData(data.message));
+    // }, []);
+
+    // useEffect(() => {
+    //     window.scrollTo(0, 0);
+    // }, []);
 
   return (
     <div className="flex flex-col min-h-screen">
+        {/* <p>{!data ? "Loading..." : data}</p> */}
         <Header />
         <HomeBody />
         <div className="flex-grow" />
